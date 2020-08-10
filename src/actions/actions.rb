6c464eb905ace3978fe2999a0d2bef7e8c318382
jesus martinez <jesus.alberto.vk@gmail.com>
2020-08-10 01:36:44 -0500

    first commit

---
+++ src/actions/actions.rb
@@ -0,0 +1,61 @@
+module Actions
+  def self.move_snake(state)
+    next_direction = state.next_direction
+    next_position  = calc_next_position(state)
+    if position_is_valid?(state, next_position)
+      move_snake_to(state, next_position)
+    else
+      end_game(state)
+    end
+  end
+
+  private
+
+  def self.calc_next_position(state)
+    curr_position = state.snake.positions.first
+
+    case state.next_direction
+      when Model::Direction::UP    
+        return Model::Coord.new(
+          curr_position.row - 1, 
+          curr_position.col
+        ) 
+      when Model::Direction::RIGHT 
+        return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col + 1
+        ) 
+      when Model::Direction::DOWN  
+        return Model::Coord.new(
+          curr_position.row  + 1, 
+          curr_position.col
+        ) 
+      when Model::Direction::LEFT  
+        return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col - 1
+        ) 
+    end    
+
+    def self.position_is_valid?(state, position)
+      is_invalid = ((position.row >= state.grid.rows ||
+        position.row < 0) || 
+        (position.col >= state.grid.cols ||
+        position.col < 0))
+      return false if is_invalid
+      return !(state.snake.positions.include? position)
+    end
+
+    def self.move_snake_to(state, next_position)
+      new_positions =  [next_position] + state.snake.positions[0...-1]
+      state.snake.positions = new_positions
+      state
+    end
+
+    def self.end_game(state)
+      state.game_finished = true
+      state
+    end
+
+  end
+end
---
+++ src/view/ruby2d.rb
@@ -0,0 +1,52 @@
+require "ruby2d"
+
+module View
+	class Ruby2dView
+
+		def initialize
+				@pixel_size = 50
+		end
+
+		def start(state)
+			extend Ruby2D::DSL
+				set(
+						title: "Snake",
+						width: @pixel_size * state.grid.cols,
+						height: @pixel_size * state.grid.rows
+				)
+				show
+		end
+
+		def render(state)
+			render_food(state)
+			render_snake(state)
+		end
+
+		private
+
+		def render_food(state)
+				extend Ruby2D::DSL
+				food = state.food
+				Square.new(
+					x: food.col * @pixel_size,
+					y: food.row * @pixel_size,
+					size: @pixel_size,
+					color: 'purple',
+				)
+		end
+
+		def render_snake(state)
+			extend Ruby2D::DSL
+			snake = state.snake
+			snake.positions.each do |pos|
+				Square.new(
+					x: pos.col * @pixel_size,
+					y: pos.row * @pixel_size,
+					size: @pixel_size,
+					color: 'green',
+				)
+			end
+		end
+
+	end
+end
